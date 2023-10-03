516a02a38829f7f3558d4dae10ae3e0766270ec8
Trupeshkumar Patel <trpatel2@crimson.ua.edu>
2023-10-02 23:44:04 -0500

    Added ownership

--- create_environment.sh
+++ create_environment.sh
@@ -1,3 +1,11 @@
+##
+ #	Author:	Trupeshkumar Patel
+ #	Email:	trpatel2@crimson.ua.edu
+ #	GitHub repository: 
+ #		https://github.com/TrupeshKumarPatel/ADCIRC_UAHPC.git
+ #	Last update:
+ #		October 2, 2023
+ ##
 #! /bin/bash
 
 # test if sourced or executed
--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
+##
+ #	Author:	Trupeshkumar Patel
+ #	Email:	trpatel2@crimson.ua.edu
+ #	GitHub repository: 
+ #		https://github.com/TrupeshKumarPatel/ADCIRC_UAHPC.git
+ #	Last update:
+ #		October 2, 2023
+ ##
 #!/bin/bash
-
 echo "==========================================================="
 echo "-- Welcome to ADCIRC installtion on UAHPC"
 echo "==========================================================="
